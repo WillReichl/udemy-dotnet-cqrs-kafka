8a67f56a71192bbc1334b9c20b642f4f7ee62084
Reichl <William.Reichl@dcsg.com>
2023-05-29 10:59:17 -0400

    End of Section 5: Aggregates

--- SM-Post/Post.Common/Events/CommentAddedEvent.cs
+++ SM-Post/Post.Common/Events/CommentAddedEvent.cs
@@ -9,7 +9,7 @@ public class CommentAddedEvent : BaseEvent
     }
 
     public Guid CommentId { get; set; }
-    public string Comment { get; set; }
-    public string Username { get; set; }
+    public required string Comment { get; set; }
+    public required string Username { get; set; }
     public DateTime CommentDate { get; set; }
 }
