2c73e5f0ea750a14f26b8f68cf64e4b316de39c5
Will-Reichl <William.Reichl@dcsg.com>
2023-08-30 07:04:08 -0400

    Section 14 - Part 1

--- SM-Post/Post.Query/Post.Query.Api/Queries/FindPostsByAuthorQuery.cs
+++ SM-Post/Post.Query/Post.Query.Api/Queries/FindPostsByAuthorQuery.cs
@@ -1,6 +1,8 @@
+using CQRS.Core.Queries;
+
 namespace Post.Query.Api.Queries;
 
-public class FindPostsByAuthorQuery
+public class FindPostsByAuthorQuery : BaseQuery
 {
     public required string Author { get; set; }
 }
